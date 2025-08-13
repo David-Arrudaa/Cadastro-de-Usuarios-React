79a601890ec10b61ebbece2c14da4ad3706e386c
David_Arruda <david.viniciusarruda@gmail.com>
2025-08-13 16:59:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Title } from './styles.js'
+import { Container } from './styles.js'
+
+
+
+function App() {
+
+  return (
+    <Container>
+      <h1>Ola</h1>
+      <Title>Ok React</Title>
+    </Container>
+  )
+}
+
+export default App
+
---
+++ src/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    background: blue;
+    border: 10px solid black;
+    width: 250px;
+    display: flex;
+    justify-content: center;
+    justify-content: space-around;
+
+    h1 {
+        color: green;
+        font-size: 40px;
+    }
+`
+export const Title = styled.h2`
+    color: red;
+    display: flex;
+    align-items: center;
+`
