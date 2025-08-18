a084efbacf55f9026a70027368649c97550e9b9c
David_Arruda <david.viniciusarruda@gmail.com>
2025-08-18 19:53:05 -0300

    Adiciona PropTypes ao projeto

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,15 @@
+import PropTypes from 'prop-types'
 import { Button } from './styles.js'
 
 function DefaultButton({ children, ...props }) {
 
-
     return (
         <Button {...props}>{children}</Button>
     )
+}
+
+DefaultButton.prototype = {
+    children: PropTypes.node.isRequired
 }
 
 export default DefaultButton
